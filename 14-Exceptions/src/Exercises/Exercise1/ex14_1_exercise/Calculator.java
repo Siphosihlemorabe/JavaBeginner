51d6147e2c094ba070cb17df77a02c7955dca147
Siphosihlemorabe <152267979+Siphosihlemorabe@users.noreply.github.com>
2024-10-11 14:48:40 +0200

    Add files via upload

---
+++ 14-Exceptions/src/Exercises/Exercise1/ex14_1_exercise/Calculator.java
@@ -0,0 +1,11 @@
+package Exercises.Exercise1.ex14_1_exercise;
+
+public class Calculator {
+    public int add(int x, int y){
+        return x + y;
+    }
+    // This method could throw an ArithmeticException
+    public double divide(int x, int y){
+        return x / y;
+    }
+}
